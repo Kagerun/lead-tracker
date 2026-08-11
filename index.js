dc42e1bc5e910fd711903cb799a0950396d2a36e
Kagerun <timur.panayirci@gmail.com>
2026-08-12 00:25:14 +0300

    initial version

---
+++ index.js
@@ -0,0 +1,17 @@
+const inputbtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+let myLeads = [];
+
+
+inputbtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value);
+    inputEl.value="";
+    renderLeads();
+})
+function renderLeads(){
+    let listItems = "";
+    for (let i = 0; i<myLeads.length;i++){
+        listItems += "<li>"+myLeads[i]+"</li>";}
+    ulEl.innerHTML= listItems;
+}
